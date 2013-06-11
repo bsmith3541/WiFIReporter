a92925d7f2a04e17eeaa2756d45b9002c6884c6b
Brandon Smith <brandon.smith@yale.edu>
2013-06-11 14:04:45 -0400

    Initial Commit

---
+++ WifiReporter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WifiReporter
+//
+//  Created by Brandon Smith on 6/11/13.
+//  Copyright (c) 2013 STC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WifiReporter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WifiReporter
+//
+//  Created by Brandon Smith on 6/11/13.
+//  Copyright (c) 2013 STC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
